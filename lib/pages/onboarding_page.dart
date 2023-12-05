4720a285e036153246dbf1d30233c76437245402
angeladrianmartinezbernardino <angeladrianmartinezbernardino@gmail.com>
2023-12-04 20:47:33 -0600

    Por el momento está bien.

--- lib/pages/onboarding_page.dart
+++ lib/pages/onboarding_page.dart
@@ -10,7 +10,7 @@ class OnboardingPage extends StatelessWidget {
   final data = [
     CardPlanetData(
       title: "Presentación de la escuela",
-      subtitle: "The night sky has much to offer to those who seek its mystery.",
+      subtitle: "El Instituto Tecnológico de Celaya (ITC) es una institución pública de educación superior ubicada en Celaya, Guanajuato, México. Se especializa en ofrecer programas académicos en el campo de la ingeniería, tecnología y ciencias aplicadas.",
       image: const AssetImage("assets/ITC.jpg"),
       backgroundColor: const Color.fromARGB(255, 0, 56, 1),
       titleColor: Colors.pink,
@@ -19,7 +19,7 @@ class OnboardingPage extends StatelessWidget {
     ),
     CardPlanetData(
       title: "Presentación de la carrera",
-      subtitle: "An endless number of galaxies means endless possibilities.",
+      subtitle: "La Ingeniería en Sistemas Computacionales es una carrera que se enfoca en el diseño, programación y mantenimiento de sistemas de software complejos. Busca formar profesionales capaces de analizar y solucionar problemas utilizando la tecnología de información y comunicaciones.",
       image: const AssetImage("assets/ISC.jpg"),
       backgroundColor: const Color.fromARGB(255, 0, 148, 2),
       titleColor: Colors.purple,
@@ -28,7 +28,7 @@ class OnboardingPage extends StatelessWidget {
     ),
     CardPlanetData(
       title: "Instalaciones y demás",
-      subtitle: "The sky dome is a beautiful graveyard of stars.",
+      subtitle: "Las instalaciones del Instituto Tecnológico de Celaya destacan por su moderna arquitectura y espacios amplios diseñados para el aprendizaje y la convivencia estudiantil.",
       image: const AssetImage("assets/Instalaciones del ITC campus 2.jpg"),
       backgroundColor: const Color.fromARGB(255, 15, 170, 1),
       titleColor: Colors.yellow,
